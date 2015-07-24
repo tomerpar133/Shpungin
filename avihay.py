b597caa0fc19b228f9295e85b93299b12f8875e1
Avihay <avihay@windowslive.com>
2015-07-24 17:03:43 +0300

    + Added some logic

--- avihay.py
+++ avihay.py
@@ -1,1 +1,12 @@
-__author__ = 'tomer'
+__author__ = 'Avihay'
+
+import os
+
+
+def organize_files(file_groups, out_dir):
+    for group in file_groups:
+        for file in group:
+            filename, file_extension = os.path.splitext(file)
+            if not os.path.isdir(os.path.join(out_dir, file_extension)):
+                os.mkdir(os.path.join(out_dir, file_extension))
+            os.rename(file, os.path.join(out_dir, filename, file_extension))
